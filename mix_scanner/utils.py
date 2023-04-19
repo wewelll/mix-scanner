733b6b5482ac7dc727a53b58ca89655003716047
Samuel Briole <samuel.briole@gmail.com>
2023-04-19 14:07:36 +0200

    format tracklist

--- mix_scanner/utils.py
+++ mix_scanner/utils.py
@@ -1,3 +1,6 @@
+from application.scanner import ScannedTrack
+
+
 def format_milliseconds(ms: int) -> str:
     seconds = (ms / 1000) % 60
     seconds = int(seconds)
@@ -6,3 +9,11 @@ def format_milliseconds(ms: int) -> str:
     hours = (ms / (1000 * 60 * 60)) % 24
 
     return "%d:%d:%d" % (hours, minutes, seconds)
+
+
+def format_track(track: ScannedTrack) -> str:
+    return f"{track.artist} - {track.title} [{format_milliseconds(track.timestamp)}]"
+
+
+def format_tracklist(tracklist: list[ScannedTrack]):
+    return "\n".join([format_track(track) for track in tracklist])
